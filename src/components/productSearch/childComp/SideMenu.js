4f13fc86c5f61df4f5efa0647f6ddb5802fd47a7
Michael Sean De La Riva <michaeldlr17@gmail.com>
2022-02-19 00:24:58 -0800

    styled up the side menu, which is essentially the same as the mobile filtermenu.  Added some categories, and changed the size of the selector icon.  I need to be careful changing the core classes of bootstrap, those classes are effecting the entire document.  In this case it worked fine because the menu's are identical.  committing changes and pushing to gitHub for live testing

--- src/components/productSearch/childComp/SideMenu.js
+++ src/components/productSearch/childComp/SideMenu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Accordion } from 'react-bootstrap';
+import { Accordion, Form } from 'react-bootstrap';
 import '../../../scss/components/productSearch/childComp/sideMenu.scss';
 
 function SideMenu() {
@@ -7,19 +7,95 @@ function SideMenu() {
         <Accordion className='sticky-top sideMenu' 
                    defaultActiveKey="0"
                    flush>
+            {/* ================================================================= */}
             <Accordion.Item eventKey="0">
                 <Accordion.Header>Category</Accordion.Header>
-                <Accordion.Body>Shirts</Accordion.Body>
-                <Accordion.Body>Shirts</Accordion.Body>
-                <Accordion.Body>Shirts</Accordion.Body>
-                <Accordion.Body>Shirts</Accordion.Body>
-                <Accordion.Body>Shirts</Accordion.Body>
+                <Accordion.Body className='firstChild'>Sweatshirts</Accordion.Body>
+                <Accordion.Body>Crew Neck</Accordion.Body>
+                <Accordion.Body>Hoodie</Accordion.Body>
+                <Accordion.Body>Zip Up</Accordion.Body>
+                <Accordion.Body>Basic</Accordion.Body>
             </Accordion.Item>
+            {/* ================================================================= */}
             <Accordion.Item eventKey="1">
-                <Accordion.Header>Featured</Accordion.Header>
-                <Accordion.Body>Flannels</Accordion.Body>
-                <Accordion.Body>Flannels</Accordion.Body>
-                <Accordion.Body>Flannels</Accordion.Body>
+                <Accordion.Header>Brand</Accordion.Header>
+                <Accordion.Body className='firstChild'>
+                    <Form>
+                        <Form.Group controlId='formBasicCheckbox'>
+                            <Form.Check type='checkbox' label='ADIDAS' />
+                        </Form.Group>
+                    </Form>
+                </Accordion.Body>
+                <Accordion.Body>
+                    <Form>
+                        <Form.Group controlId='formBasicCheckbox'>
+                            <Form.Check type='checkbox' label='VANS' />
+                        </Form.Group>
+                    </Form>
+                </Accordion.Body>
+                <Accordion.Body>
+                    <Form>
+                        <Form.Group controlId='formBasicCheckbox'>
+                            <Form.Check type='checkbox' label='SANTA CRUZ' />
+                        </Form.Group>
+                    </Form>
+                </Accordion.Body>
+                <Accordion.Body>
+                    <Form>
+                        <Form.Group controlId='formBasicCheckbox'>
+                            <Form.Check type='checkbox' label='BILLABONG' />
+                        </Form.Group>
+                    </Form>
+                </Accordion.Body>
+                <Accordion.Body>
+                    <Form>
+                        <Form.Group controlId='formBasicCheckbox'>
+                            <Form.Check type='checkbox' label='CONVERSE' />
+                        </Form.Group>
+                    </Form>
+                </Accordion.Body>
+                <Accordion.Body>
+                    <Form>
+                        <Form.Group controlId='formBasicCheckbox'>
+                            <Form.Check type='checkbox' label='DICKIES' />
+                        </Form.Group>
+                    </Form>
+                </Accordion.Body>
+            </Accordion.Item>
+            {/* ================================================================= */}
+            <Accordion.Item eventKey="2">
+                <Accordion.Header>Size</Accordion.Header>
+                <Accordion.Body className='firstChild'>
+                    <Form>
+                        <Form.Group controlId='formBasicCheckbox'>
+                            <Form.Check type='checkbox' label='S' />
+                        </Form.Group>
+                    </Form>
+                </Accordion.Body>
+                <Accordion.Body>
+                    <Form>
+                        <Form.Group controlId='formBasicCheckbox'>
+                            <Form.Check type='checkbox' label='M' />
+                        </Form.Group>
+                    </Form>
+                </Accordion.Body>
+                <Accordion.Body>
+                    <Form>
+                        <Form.Group controlId='formBasicCheckbox'>
+                            <Form.Check type='checkbox' label='L' />
+                        </Form.Group>
+                    </Form>
+                </Accordion.Body>
+            </Accordion.Item>
+            {/* ================================================================= */}
+            <Accordion.Item eventKey="3">
+                <Accordion.Header>Color</Accordion.Header>
+                <Accordion.Body className='firstChild'>Exclusives</Accordion.Body>
+            </Accordion.Item>
+            {/* ================================================================= */}
+            <Accordion.Item eventKey="4">
+                <Accordion.Header>Price</Accordion.Header>
+                <Accordion.Body className='firstChild'>Exclusives</Accordion.Body>
             </Accordion.Item>
         </Accordion>
     );
